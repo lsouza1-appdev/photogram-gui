3c7552b16af7de83fabfa02ee8e5d103cd34f6ab
lsouza1 <lsouza@uchicago.edu>
2021-11-10 01:50:11 +0000

    82%

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -37,4 +37,38 @@ class PhotosController < ApplicationController
   new_photo.save
     redirect_to('/photos/'+ new_photo.id.to_s)
   end 
+
+  def update
+    input_image = params.fetch("input_image")
+    input_caption = params.fetch("input_caption")
+    the_id = params.fetch("photo_id")
+
+    matching_photo = Photo.where({:id => the_id}).first
+
+    matching_photo.image = input_image
+    matching_photo.caption = input_caption
+    matching_photo.save
+
+    redirect_to("/photos/"+the_id.to_s)
+
+  end 
+      
+  def create_comment
+
+    input_id = params.fetch("input_photo_id")
+    input_author_id = params.fetch("input_author_id")
+    input_comment = params.fetch("input_body")
+    
+    new_comment = Comment.new
+
+    new_comment.photo_id = input_id
+    new_comment.author_id = input_author_id
+    new_comment.body = input_comment
+    new_comment.save
+    redirect_to("/photos/"+input_id.to_s)
+  
+  end 
+    
+      
+   
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
     get("/photos/:photo_id", {:controller => "photos",:action => "show"})
     get("/delete_photo/:photo_id", {:controller => "photos", :action => "toast"})
     get("/insert_photo_record", {:controller => "photos", :action => "create"})
+    get("/update_photo/:photo_id",{:controller=> "photos", :action => "update"})
+    get("/insert_comment_record", {:controller => "photos", :action => "create_comment"})
+  
   end
